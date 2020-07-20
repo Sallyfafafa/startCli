cb661c2b2e51c75268d3c714118695793a7a41b8
Sally <sally@SallydeMacBook-Pro.local>
2020-07-20 20:07:45 +0800

    init

---
+++ bin/kkb.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const program = require("commander");
+program.version(require("../package.json").version)
+program
+    .command('init <name>') // 命令语法
+    .description('init project  ') // 命令的描述
+    .action(
+        require('../lib/init.js')
+    )
+
+program.parse(process.argv) // 解析主进程的参数 （整个命令行解析和定制的过程 传入的就是主进程的参数）
