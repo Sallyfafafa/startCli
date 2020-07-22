54d5c24a5b0ef3cebc336b97de8cd682765f8b71
Sally <sally@SallydeMacBook-Pro.local>
2020-07-22 15:31:08 +0800

    fresh

--- bin/kkb.js
+++ bin/kkb.js
@@ -7,5 +7,10 @@ program
     .action(
         require('../lib/init.js')
     )
-
+program
+    .command('refresh') // 命令语法
+    .description('refresh routers an menu project  ') // 命令的描述
+    .action(
+        require('../lib/refresh.js')
+    )
 program.parse(process.argv) // 解析主进程的参数 （整个命令行解析和定制的过程 传入的就是主进程的参数）
